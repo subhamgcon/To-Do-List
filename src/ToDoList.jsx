88576e8a2b45fee0a4920d7545dc9f9419c108b3
Subham Ghosh <74487802+subhamgcon@users.noreply.github.com>
2025-02-27 19:33:25 +0530

    Add files via upload

---
+++ src/ToDoList.jsx
@@ -0,0 +1,62 @@
+import React, {useState} from "react"
+
+function ToDoList(){
+  const [tasks,setTasks] = useState(["Eat Breakfast", "Have a Cold Shower", "Go to Gym"]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event){
+    setNewTask(event.target.value);
+  }
+
+  function addTask(){
+    if(newTask.trim() !==""){
+    setTasks(t => [...t,newTask]);
+    setNewTask("");
+    }
+}
+  function removeTask(index){
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+
+  }
+  function moveTaskUp(index){
+    if(index > 0){
+        const updatedTasks =[...tasks];
+        [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index - 1],updatedTasks[index]];
+        setTasks(updatedTasks);
+    }
+  }
+  function moveTaskDown(index){
+    if(index < tasks.length){
+    const updatedTasks =[...tasks];
+    [updatedTasks[index],updatedTasks[index+1]] = [updatedTasks[index + 1],updatedTasks[index]];
+    setTasks(updatedTasks);
+    }
+
+  }
+
+  return(
+    <div className="to-do-list">
+        <h2>Donarama</h2>
+        <input type="text" placeholder="Enter your task..." onChange={handleInputChange}/>
+        <button className="add-button" onClick={addTask}>Add</button>
+        <ol>
+            {tasks.map((tasks,index) => 
+              <li key={index}>
+                <span className="text">{tasks}</span>
+                <button className="delete-button" onClick={() => removeTask(index)}> Delete </button> 
+                <button className="move-button" onClick={() => moveTaskUp(index)}> Up </button> 
+                <button className="move-button" onClick={() => moveTaskDown(index)}> Down </button> 
+                
+
+                 </li>
+            )}
+        </ol>
+     
+
+        
+    </div>
+  );
+}
+
+export default ToDoList
